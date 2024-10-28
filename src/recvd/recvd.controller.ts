23403e5a4ec39ca1f12255a54d8b21515aa7e7bc
laixuexin <1487537353@qq.com>
2024-10-28 17:51:58 +0800

    fix: 需要解析source

--- src/recvd/recvd.controller.ts
+++ src/recvd/recvd.controller.ts
@@ -12,10 +12,14 @@ export class RecvdController {
     const type = body.type;
     const isMentioned = body.isMentioned === '1';
     const content = (body.content as string).replace('@木小博士 ', '').trim();
-    const isRoom = !!body.source?.room?.id;
-    const fromUser = body.source?.from?.payload?.name;
-
-    console.log(body.isMentioned, body.source)
+    let source = {} as Record<string, any>;
+    try {
+      source = JSON.parse(body.source);
+    } catch (error) {
+      console.error('source 解析失败', error);
+    }
+    const isRoom = !!source?.room?.id;
+    const fromUser = source?.from?.payload?.name;
 
     return this.appService.router({
       type,
