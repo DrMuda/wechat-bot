80aa947e25559a2ffb1e256026c816f1b3e8256e
laixuexin <1487537353@qq.com>
2024-11-01 17:59:59 +0800

    feat: docker命令调整

--- startAndReload.sh
+++ startAndReload.sh
@@ -1,5 +1,11 @@
+OLD_IMAGE_ID=$(docker images -q wechat-bot)
+LABEL=wechat-bot
+
+docker build --label "$LABEL" -t wechat-bot .
 docker stop wechat-bot
 docker rm wechat-bot
-docker rmi wechat-bot
-docker build -t wechat-bot .
+if [ -n "$OLD_IMAGE_ID" ] && [ "$OLD_IMAGE_ID" != "$(docker images -q wechat-bot:latest)" ]; then
+  docker rmi "$OLD_IMAGE_ID"
+fi
 docker run -d --name wechat-bot --network my-network -e TZ='Asia/Shanghai' -p 3000:3000 -v /www/wwwroot/wechat-bot-data:/wechat-bot-data wechat-bot
+docker builder prune --filter "label=$LABEL"
