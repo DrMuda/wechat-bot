7f122b088de2beefcf40f93983326e7a17d7f890
laixuexin <1487537353@qq.com>
2024-11-01 15:56:10 +0800

    fix: 镜像清理

--- startAndReload.sh
+++ startAndReload.sh
@@ -1,4 +1,5 @@
 docker stop wechat-bot
 docker rm wechat-bot
+docker rmi wechat-bot
 docker build -t wechat-bot .
 docker run -d --name wechat-bot --network my-network -e TZ='Asia/Shanghai' -p 3000:3000 -v /www/wwwroot/wechat-bot-data:/wechat-bot-data wechat-bot
