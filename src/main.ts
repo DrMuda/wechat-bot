b1983975732a27b715927cf96f6d5d1388bb602a
laixuexin <1487537353@qq.com>
2024-12-25 12:04:11 +0800

    feat: test pixiv

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './recvd/recvd.module';
+import { defaultCatchFetch } from 'src/utils';
+import Pixiv from 'pixiv.ts';
+import * as fs from 'fs';
+import * as path from 'path';
+import { saveDataDir } from 'src/config';
 
 const dayjs = require('dayjs');
 const utc = require('dayjs/plugin/utc');
@@ -13,7 +18,35 @@ console.log('时区', dayjs.tz.guess());
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(process.env.NODE_ENV)
+  console.log(process.env.NODE_ENV);
   await app.listen(process.env.PORT ?? 3000);
+  const pixiv = await Pixiv.refreshLogin(
+    '3SoGdGWVuMSh4P6Huol_6qu-as5wipOffqty0wGFZ6M',
+  ).catch((error) => {
+    console.log(error);
+    return null;
+  });
+  if (pixiv) {
+    let illusts = await pixiv.search.illusts({
+      word: 'gabriel dropout',
+      r18: false,
+    });
+    console.log(illusts[1]);
+    const illust = illusts[1];
+    if (illust) {
+      await pixiv.util.downloadIllust(
+        illust,
+        `${saveDataDir}/illust`,
+        'medium',
+      );
+
+      const files = fs
+        .readdirSync(path.join(__dirname, '../illust'))
+        .map((p) => {
+          return path.join(__dirname, '../illust', p);
+        });
+      console.log(files);
+    }
+  }
 }
 bootstrap();
